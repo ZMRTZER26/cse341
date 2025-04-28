3c383f93c6760b616c2216fe8a392a10bd61b56f
ZMRTZER26 <zmrtzer@byui.edu>
2025-04-28 14:50:45 -0600

    Lesson 2 Initial

--- professional-backend/app.js
+++ professional-backend/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const professionalRoutes = require('./routes/professional');
 
-app.use(express.json());
+const app = express();
+const PORT = 8080;
+
+// Middleware
+app.use(cors()); // Allow requests from your frontend
+app.use(express.json()); // Parse incoming JSON
 
 // Routes
-app.use('/api', professionalRoutes);  // All routes in professional.js will start with /api
+app.use('/', professionalRoutes);
 
 // Start server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
