1ba13bd2d568975a19c113bbf44361ba2af5cb0d
ZMRTZER26 <zmrtzer@byui.edu>
2025-05-08 18:49:19 -0600

    W03 Test

--- http_requests/controllers/contacts.js
+++ http_requests/controllers/contacts.js
@@ -1,28 +1,26 @@
 const mongodb = require('../db/connect');
 
-// Get all contacts
-const getContacts = async (req, res) => {
+const getContacts = async (requestAnimationFrame, res) => {
     try {
         const result = await mongodb.getDb().db('assignments').collection('contacts').find();
         const data = await result.toArray();
         res.status(200).json(data);
     } catch (err) {
-        console.error('Error fetching data:', err.message);
+        console.error('Error fetching dta:', err.message);
         res.status(500).json({ message: 'Server error' });
     }
 };
 
-// Get a contact by ID
-const getContactById = async (req, res) => {
-    const { id } = req.params; // Get the contactId from the URL parameter
+const { ObjectId } = require('mongodb');
 
+const getSingleContact = async (req, res) => {
     try {
-        const result = await mongodb.getDb().db('assignments').collection('contacts').findOne({ _id: new mongodb.ObjectId(id) });
-        
+        const contactId = new ObjectId(req.params.id);
+        const result = await mongodb.getDb().db('assignments').collection('contacts').findOne({ _id: contactId });
         if (result) {
             res.status(200).json(result);
         } else {
-            res.status(404).json({ message: 'Contact not found.' });
+            res.status(404).json({ message: 'Contact not found' });
         }
     } catch (err) {
         console.error('Error fetching contact:', err.message);
@@ -30,18 +28,24 @@ const getContactById = async (req, res) => {
     }
 };
 
-// Create a new contact (POST)
 const createContact = async (req, res) => {
-    const { firstName, lastName, email, favoriteColor, birthday } = req.body;
-
-    if (!firstName || !lastName || !email || !favoriteColor || !birthday) {
-        return res.status(400).json({ message: 'All fields are required.' });
-    }
-
     try {
-        const newContact = { firstName, lastName, email, favoriteColor, birthday };
+        const { firstName, lastName, email, favoriteColor, birthday } = req.body;
+
+        if (!firstName || !lastName || !email || !favoriteColor || !birthday) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+
+        const newContact = {
+            firstName,
+            lastName,
+            email,
+            favoriteColor,
+            birthday
+        };
+
         const result = await mongodb.getDb().db('assignments').collection('contacts').insertOne(newContact);
-        
+
         res.status(201).json({ id: result.insertedId });
     } catch (err) {
         console.error('Error creating contact:', err.message);
@@ -49,41 +53,42 @@ const createContact = async (req, res) => {
     }
 };
 
-// Update a contact by ID (PUT)
 const updateContact = async (req, res) => {
-    const { id } = req.params; // Get the contactId from the URL parameter
-    const updatedData = req.body;
-
     try {
+        const contactId = new ObjectId(req.params.id);
+        const updatedContact = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            favoriteColor: req.body.favoriteColor,
+            birthday: req.body.birthday
+        };
+
         const result = await mongodb.getDb().db('assignments').collection('contacts').updateOne(
-            { _id: new ObjectId(req.params.id) },
-            { $set: updatedData }
+            { _id: contactId },
+            { $set: updatedContact }
         );
-        
-        if (result.modifiedCount > 0) {
-            res.status(204).send(); // No content to send
-        } else {
-            res.status(404).json({ message: 'Contact not found or no changes made.' });
+
+        if (result.matchedCount === 0) {
+            return res.status(404).json({ message: 'Contact not found' });
         }
+
+        res.status(204).send(); // Successfully updated, no content to return
     } catch (err) {
         console.error('Error updating contact:', err.message);
         res.status(500).json({ message: 'Server error' });
     }
 };
 
-// Delete a contact by ID (DELETE)
 const deleteContact = async (req, res) => {
-    const { id } = req.params; // Get the contactId from the URL parameter
-
     try {
-        const result = await mongodb.getDb().db('assignments').collection('contacts').deleteOne(
-            { _id: new ObjectId(req.params.id) }
-        );
-        
-        if (result.deletedCount > 0) {
-            res.status(200).json({ message: 'Contact deleted successfully.' });
+        const contactId = new ObjectId(req.params.id);
+        const result = await mongodb.getDb().db('assignments').collection('contacts').deleteOne({ _id: contactId });
+
+        if (result.deletedCount === 1) {
+            res.status(200).json({ message: 'Contact successfully deleted' });
         } else {
-            res.status(404).json({ message: 'Contact not found.' });
+            res.status(404).json({ message: 'Contact not found' });
         }
     } catch (err) {
         console.error('Error deleting contact:', err.message);
@@ -91,4 +96,4 @@ const deleteContact = async (req, res) => {
     }
 };
 
-module.exports = { getContacts, getContactById, createContact, updateContact, deleteContact };
+module.exports = { getContacts, getSingleContact, createContact, updateContact, deleteContact };
--- http_requests/routes/contacts.js
+++ http_requests/routes/contacts.js
@@ -2,19 +2,10 @@ const express = require('express');
 const router = express.Router();
 const contactsController = require('../controllers/contacts');
 
-// GET all contacts
 router.get('/', contactsController.getContacts);
-
-// GET a contact by ID
-router.get('/:id', contactsController.getContactById);
-
-// POST a new contact
+router.get('/:id', contactsController.getSingleContact);
 router.post('/', contactsController.createContact);
-
-// PUT (update) a contact by ID
 router.put('/:id', contactsController.updateContact);
-
-// DELETE a contact by ID
 router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
