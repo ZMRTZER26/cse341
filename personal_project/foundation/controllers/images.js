db967828f32a91f47e3754888572c1961d68879f
ZMRTZER26 <zmrtzer@byui.edu>
2025-05-23 17:00:31 -0600

    W05 Test

--- personal_project/foundation/controllers/images.js
+++ personal_project/foundation/controllers/images.js
@@ -24,7 +24,7 @@ const linkImage = async (req, res) => {
       return res.status(400).json({ message: 'All fields are required' });
     }
 
-    const newVtuber = {
+    const newImage = {
       url,
       artist,
       vtuberId,
@@ -33,7 +33,7 @@ const linkImage = async (req, res) => {
     const result = await mongodb.getDb().db('personal_project').collection('images').insertOne(newImage);
     res.status(201).json({ id: result.insertedId });
   } catch (err) {
-    console.error('Error creating vtuber:', err.message);
+    console.error('Error linking image:', err.message);
     res.status(500).json({ message: 'Server error' });
   }
 };
