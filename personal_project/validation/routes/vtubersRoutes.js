fefb1192139876a98a54993262248ac2714e40a8
ZMRTZER26 <zmrtzer@byui.edu>
2025-05-30 16:22:30 -0600

    W06 Test

--- personal_project/validation/routes/vtubersRoutes.js
+++ personal_project/validation/routes/vtubersRoutes.js
@@ -5,41 +5,7 @@ const { validateVtuber } = require("../middleware/validate");
 
 router.get("/", vtubersController.getVtubers);
 router.get("/:id", vtubersController.getSingleVtuber);
-// #swagger.tags = ['Vtubers']
-// #swagger.description = 'Create a new vtuber.'
-/* #swagger.parameters['obj'] = {
-    in: 'body',
-    description: 'Vtuber information',
-    required: true,
-    schema: {
-      name: "Hoshimachi Suisei",
-      debut: "2018-03-22",
-      agency: "Hololive",
-      bio: "An idol vtuber and singer.",
-      language: "Japanese",
-      status: "Active",
-      channel: "https://www.youtube.com/channel/UC5CwaMl1eIgY8h02uZw7u8A"
-    }
-  }
-*/
 router.post("/", validateVtuber, vtubersController.createVtuber);
-// #swagger.tags = ['Vtubers']
-// #swagger.description = 'Update an existing vtuber.'
-/* #swagger.parameters['obj'] = {
-    in: 'body',
-    description: 'Updated vtuber info',
-    required: true,
-    schema: {
-      name: "Shirakami Fubuki",
-      debut: "2018-06-01",
-      agency: "Hololive",
-      bio: "A cheerful fox vtuber.",
-      language: "Japanese",
-      status: "Active",
-      channel: "https://www.youtube.com/channel/UCdn5BQ06XqgXoAxIhbqw5Rg"
-    }
-  }
-*/
 router.put("/:id", validateVtuber, vtubersController.updateVtuber);
 router.delete("/:id", vtubersController.deleteVtuber);
 
