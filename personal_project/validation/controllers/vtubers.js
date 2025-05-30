5124eb5a8dc27634753c90785c78f889f8c1e132
ZMRTZER26 <zmrtzer@byui.edu>
2025-05-30 17:00:16 -0600

    W06 Test

--- personal_project/validation/controllers/vtubers.js
+++ personal_project/validation/controllers/vtubers.js
@@ -21,39 +21,63 @@ const getSingleVtuber = async (req, res, next) => {
   }
 };
 
-const createVtuber = async (req, res, next) => {
+const createVtuber = async (req, res) => {
   try {
-    const requiredFields = [
-      "name",
-      "debut",
-      "agency",
-      "bio",
-      "language",
-      "status",
-      "channel",
-    ];
-    for (const field of requiredFields) {
-      if (!req.body[field]) {
-        return res
-          .status(400)
-          .json({ message: `Missing required field: ${field}` });
-      }
+    const { name, debut, agency, bio, language, status, channel } = req.body;
+
+    if (
+      !name ||
+      !debut ||
+      !agency ||
+      !bio ||
+      !language ||
+      !status ||
+      !channel
+    ) {
+      return res.status(400).json({ message: "All fields are required" });
     }
 
-    const newVtuber = await Vtuber.create(req.body);
-    res.status(201).json(newVtuber);
+    const newVtuber = new Vtuber({
+      name,
+      debut,
+      agency,
+      bio,
+      language,
+      status,
+      channel,
+    });
+    const savedVtuber = await newVtuber.save();
+
+    res.status(201).json(savedVtuber);
   } catch (err) {
     next(err);
   }
 };
 
-const updateVtuber = async (req, res, next) => {
+const updateVtuber = async (req, res) => {
   try {
     const { id } = req.params;
-    const updatedVtuber = await Vtuber.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const { name, debut, agency, bio, language, status, channel } = req.body;
+
+    if (
+      !name ||
+      !debut ||
+      !agency ||
+      !bio ||
+      !language ||
+      !status ||
+      !channel
+    ) {
+      return res
+        .status(400)
+        .json({ message: "All fields are required for update" });
+    }
+
+    const updatedVtuber = await Vtuber.findByIdAndUpdate(
+      id,
+      { name, debut, agency, bio, language, status, channel },
+      { new: true, runValidators: true }
+    );
 
     if (!updatedVtuber) {
       return res.status(404).json({ message: "Vtuber not found" });
