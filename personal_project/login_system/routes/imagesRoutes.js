a81ce0d5ddaef406a024b6b885bd2b0c9426a999
ZMRTZER26 <zmrtzer@byui.edu>
2025-06-06 14:42:00 -0600

    authRoute for imagesRoutes

--- personal_project/login_system/routes/imagesRoutes.js
+++ personal_project/login_system/routes/imagesRoutes.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const imagesController = require("../controllers/images");
 const { validateImage } = require("../middleware/validate");
+const ensureAuthenticated = require("../middleware/authCheck");
+
+router.post(
+  "/",
+  ensureAuthenticated,
+  validateImage,
+  imagesController.linkImage
+);
 
 router.get("/", imagesController.getImages);
 router.get("/:id", imagesController.getSingleImage);
