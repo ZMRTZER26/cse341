7762c7b824c3751d319e16f6d885e6109efab436
ZMRTZER26 <zmrtzer@byui.edu>
2025-06-27 18:21:00 -0600

    Render Update

--- buddy_reading_tracker/server.js
+++ buddy_reading_tracker/server.js
@@ -3,7 +3,7 @@ const { connect } = require("./config/db");
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger/swagger-output.json");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
