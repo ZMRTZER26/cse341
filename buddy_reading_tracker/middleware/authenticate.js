256c494f9e19bedee58afbe15ebf30006a484de4
ZMRTZER26 <zmrtzer@byui.edu>
2025-07-12 21:00:34 -0600

    authentication tweak

--- buddy_reading_tracker/middleware/authenticate.js
+++ buddy_reading_tracker/middleware/authenticate.js
@@ -10,10 +10,7 @@ const authenticate = (req, res, next) => {
   const token = authHeader;
 
   try {
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET || "default-secret"
-    );
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     req.user = decoded;
     next();
