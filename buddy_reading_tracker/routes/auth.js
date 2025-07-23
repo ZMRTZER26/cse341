2ea2ddca47816c90427dc38063125f9506941e6e
ZMRTZER26 <zmrtzer@byui.edu>
2025-07-22 20:34:56 -0600

    Undo Logout Fix

--- buddy_reading_tracker/routes/auth.js
+++ buddy_reading_tracker/routes/auth.js
@@ -14,11 +14,8 @@ router.get("/google/callback", passport.authenticate("google"), (req, res) => {
 });
 
 router.get("/logout", (req, res) => {
-  const googleLogoutURL = "https://accounts.google.com/Logout";
-  const redirectBack = "https://buddy-reading-tracker.onrender.com/api-docs";
-  res.redirect(
-    `${googleLogoutURL}?continue=${encodeURIComponent(redirectBack)}`
-  );
+  req.logout();
+  res.redirect("/");
 });
 
 module.exports = router;
